500cd42167c81280c6541de1a15b85ce327714b2
Russell Jarvis <russelljarvis@protonmail.com>
2021-03-05 09:56:41 +1100

    update heroku build

--- setup.sh
+++ setup.sh
@@ -4,18 +4,7 @@
 # download and install latest geckodriver for linux or mac.
 # required for selenium to drive a firefox browser.
 sudo apt-get update
-sudo python3 -m pip install -r requirements.txt
 sudo python3 -m pip install seaborn
-sudo python3 -m pip install bs4
-sudo python3 -m pip install natsort dask plotly tabulate
-#sudo python3 -m conda install -c pyviz holoviews bokeh
-#sudo conda install -c pyviz holoviews bokeh
-#sudo python3 -m pip install git+https://github.com/pyviz/holoviews.git
-
-# hack package installs:
-
-#git clone https://github.com/pyviz/holoviews.git
-#cd holoviews; sudo pip install -e .; cd ..;
 
 apt-get install -y cpp gcc
 apt-get install -y libx11-6 python-dev git build-essential
@@ -28,19 +17,22 @@ conda config --set always_yes true
 conda config --set quiet true
 conda install conda-build
 pip install pip --upgrade;
+sudo python3 -m pip install -r requirements.txt
+
 conda install numpy;
 conda install numba;
 conda install dask;
 pip install tables
 pip install scipy==1.5.4
-pip install coverage
+#pip install coverage
 pip install cython
-pip install asciiplotlib;
+#pip install asciiplotlib;
 pip install ipfx
 pip install streamlit
 pip install sklearn
 pip install seaborn
 pip install frozendict
+
 # pip install plotly
 pip install allensdk==0.16.3
 pip install --upgrade colorama
