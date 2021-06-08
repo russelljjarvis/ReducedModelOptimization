fa190c50a75951fcba47f979875b91729ebb11c2
Russell Jarvis <russelljarvis@protonmail.com>
2021-06-08 22:50:42 +1000

    update

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
 import setuptools; setuptools.setup()
-
 extras_require={'BluePyOpt': ['BluePyOpt @ git+git+https://github.com/russelljjarvis/BluePyOpt.git@neuronunit_reduced_cells']}
+extras_require1={'neuronunit': ['neuronunit @ git+https://github.com/russelljjarvis/neuronunit.git@optimization']}
+extras_require.update(extras_require1)
